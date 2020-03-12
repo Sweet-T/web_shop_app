d8fcedf0f8ba2824d267a36381405af487f59f1e
CoderOfRat <1404616446@qq.com>
2020-03-12 12:40:00 +0800

    购物车功能、订单列表、地址添加完成

---
+++ src/views/orders/Addresslist.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <van-address-list
+      v-model="chosenAddressId"
+      :list="localAddress"
+      default-tag-text="默认"
+      @add="onAdd"
+      @edit="onEdit"
+      @click-item="chooseAddress"
+    />
+  </div>
+</template>
+
+<script>
+import areaList from "../../../public/area.js";
+export default {
+  data() {
+    return {
+      chosenAddressId: '1',
+      localAddress: [],
+      orderList: {},
+    }
+  },
+  methods: {
+    onAdd() {
+      this.$router.push('/address/{}')
+    },
+    // 当点击编辑按钮时
+    onEdit(item, index) {
+      console.dir(item)
+      console.dir(index)
+      if(!item) {
+        item = {};
+      }
+      item.id = index;
+      if(!item) {
+        item = {};
+      }
+      this.$router.push(`/address/${JSON.stringify(item)}`)
+    },
+    // 当选中地址时
+    chooseAddress(item) {
+      this.$router.push(`/order/${JSON.stringify([this.orderList,item])}`)
+    }
+  },
+  created() {
+    if(!JSON.parse(window.localStorage.getItem('addresses'))) {
+      window.localStorage.setItem('addresses',JSON.stringify([]));
+    }
+    this.localAddress = JSON.parse(window.localStorage.getItem('addresses'))
+    this.orderList = JSON.parse(this.$route.params.data)[0]
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
